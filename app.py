41d2c2dd73a6d3143ca663a69eb59a1440f0b56f
bharathm5291-gif <bharathm5291@gmail.com>
2026-02-24 22:29:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,8 +42,11 @@ def add_crop():
     db.session.commit()
     return redirect(url_for('home'))
 
+# --- THE FIX ---
+# This line creates the "Shelves" (tables) the moment Render starts the app
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
